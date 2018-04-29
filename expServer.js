d7d4bc2e33525ae9aaab90b4544d1bd79473a932
MaryCassieOdie <31669631+MaryCassieOdie@users.noreply.github.com>
2018-04-28 20:40:25 -0500

    post/get search res

--- expServer.js
+++ expServer.js
@@ -107,13 +107,13 @@ app.get('/bookPage:isbn', function (req, res) {
 });
 
 //Search result page
-app.post('/searchRes', function (req, res) {
+app.get('/searchRes:query', function (req, res) {
     if (!req.session.user)
     res.render('thanks', {
         message: "You are currently not logged in, please log in or sign up at the top of the page to continue"
     });
     else {
-        phrase = req.body.query;
+        phrase = req.params.query.slice(1);
         let sql = 'SELECT * FROM books ' +
         'WHERE author LIKE "%' + phrase + '%" OR ' +
         'title LIKE "%' + phrase + '%" OR ' +
